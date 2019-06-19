f3aef951e5bce843e377d36fbccabb16204e0a32
bigudam <dharma.herrera@gmail.com>
2019-06-19 15:24:25 -0400

    leyendo archivos

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+// fs=fine systemnpm install --
+const fs= require('fs');
+
+const marked= require('marked');
+// fs.readFile(./)
+const links= (path)=>{
+  fs.readFile(path, 'utf8', (err, data)=>{
+  if(err){
+throw err
+  }
+
+
+let links= []//es para guardar los documentos
+const renderer = new  marked.Renderer();//new es para crear un nueva instancia
+renderer.link= function(href, title, text){
+links.push({
+  href:href,
+  text:text,
+  file:path,
+})
+}
+marked(data, {renderer:renderer})
+console.log(links)
+})
+}
+
+console.log(links('./prueba.md'));
+
