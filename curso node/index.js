d7172169f3b9eeb60a6608f62d874cb81c5b82f5
olgamperez <ing.olgaperez@gmail.com>
2019-07-05 17:26:59 -0400

    curso de node

---
+++ curso node/index.js
@@ -0,0 +1,16 @@
+const math = require('./index.js');
+
+
+
+// console.log("hola mundo!");
+// console.log(add(1, 2));
+// console.log(multiply(3, 8));
+// console.log(substract(5, 3));
+// console.log(divide(2, 0));
+console.log(math.divide(2, 4));
+
+
+
+
+
+
